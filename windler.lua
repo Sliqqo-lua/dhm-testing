d88a2f015cb853ef51102b14671c977c1efd73ca
blurred <75748702+Sliqqo-lua@users.noreply.github.com>
2022-12-15 16:22:29 -0500

    Update windler.lua

--- windler.lua
+++ windler.lua
@@ -1,6 +1,3 @@
-
---LIBARY--
-
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("windler", "Synapse")
 
